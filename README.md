096f2c6ab496519c1b385cdf3b634d44bdb9325c
Alexander <alexzhur87@mail.ru>
2023-04-28 16:11:12 +0300

    add brain_even.py; package build, publish and reinstalled

--- README.md
+++ README.md
@@ -4,3 +4,6 @@
 # Code Climate Maintainability Badge link 
 <a href="https://codeclimate.com/github/alex873110/python-project-49/maintainability"><img 
 src="https://api.codeclimate.com/v1/badges/d933dc886756d0a1eb03/maintainability" /></a>
+
+# Link to asciinema video with brain-even package test 
+https://asciinema.org/connect/2eaa6a65-5745-4524-9282-776349ae28da
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,41 @@
+
+import prompt
+from random import randint
+
+
+def is_even(number):
+    if number % 2 == 0:
+       return 'yes'
+    else:
+        return 'no'
+
+print('Welcome to the Brain Games!')
+name = prompt.string('May I have your name? ')
+print ('Hello, ' + name + '!')
+print('Answer "yes" if the number is even, otherwise answer "no".')
+
+def even_question():
+    counter = 0
+    while counter < 3:
+        num = randint(1, 1000)
+        str_num = str(num)
+        print('Question: ' + str_num)
+        answer = prompt.string('Your answer:')
+        if answer == is_even(int(str_num)):
+            print('Correct!')
+            counter += 1
+        else:
+            print("'" + answer + "' is wrong answer ;(. Correct answer was '" +
+(is_even(int(str_num))) + "'.")
+            print("Let's try again, " + name + '!')
+            break
+    if counter == 3:
+        print('Congratulations, ' + name + '!')
+
+
+def main():
+    even_question()
+
+
+if __name__ == '__main__':
+    main()
