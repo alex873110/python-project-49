abf576aaf3398c1674f87b575cf43a1dd8fc9b3a
Alexander <alexzhur87@mail.ru>
2023-04-30 19:53:16 +0300

    script brain_even now use functions from games/common_functions

--- README.md
+++ README.md
@@ -5,5 +5,8 @@
 <a href="https://codeclimate.com/github/alex873110/python-project-49/maintainability"><img 
 src="https://api.codeclimate.com/v1/badges/d933dc886756d0a1eb03/maintainability" /></a>
 
-# Link to asciinema video with brain-even package test 
+# Link to asciinema video with brain-even game test 
 https://asciinema.org/connect/2eaa6a65-5745-4524-9282-776349ae28da
+
+# Link to asciinema video with brain-calc game test
+https://asciinema.org/a/AMPisdRse5gNXlLl9vOKQKV0j
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,7 +1,7 @@
 
 import prompt
 from random import randint
-
+from brain_games.games.common_functions import hello, question, congratulation
 
 def is_even(number):
     if number % 2 == 0:
@@ -10,9 +10,7 @@ def is_even(number):
         return 'no'
 
 
-print('Welcome to the Brain Games!')
-name = prompt.string('May I have your name? ')
-print('Hello, ' + name + '!')
+hello()
 print('Answer "yes" if the number is even, otherwise answer "no".')
 
 
@@ -20,19 +18,13 @@ def even_question():
     counter = 0
     while counter < 3:
         num = randint(1, 1000)
-        str_num = str(num)
-        print('Question: ' + str_num)
-        answer = prompt.string('Your answer:')
-        if answer == is_even(int(str_num)):
-            print('Correct!')
-            counter += 1
-        else:
-            print("'" + answer + "' is wrong answer ;(. Correct answer was '"
-                  + (is_even(int(str_num))) + "'.")
-            print("Let's try again, " + name + '!')
-            break
+        expression = str(num)
+        result = is_even(num)
+        count = question(expression, result, counter)
+        counter = count
+
     if counter == 3:
-        print('Congratulations, ' + name + '!')
+        congratulation()
 
 
 def main():
