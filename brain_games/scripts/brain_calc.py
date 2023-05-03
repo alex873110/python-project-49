b1bdee900e6a77c93cd2b542c2e1b903b14076c3
Alexander <alexzhur87@mail.ru>
2023-05-03 08:37:38 +0300

    correct brain_calc.py after codeclimate test

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -3,23 +3,27 @@ from random import randint, choice
 from brain_games.games.common_functions import hello, question, congratulation
 
 
+def random_expression():
+    num1 = randint(1, 100)
+    num2 = randint(1, 100)
+    char = choice('+-*')
+    expression = str(num1) + ' ' + char + ' ' + str(num2)
+    global result
+    if char == '+':
+        result = num1 + num2
+    elif char == '-':
+        result = num1 - num2
+    else:
+        result = num1 * num2
+    return expression    
+
 def calculation():
     hello()
     global counter
     counter = 0
     print('What is the result of the expression?')
     while counter < 3:
-        num1 = randint(1, 100)
-        num2 = randint(1, 100)
-        char = choice('+-*')
-        expression = str(num1) + ' ' + char + ' ' + str(num2)
-        if char == '+':
-            result = num1 + num2
-        elif char == '-':
-            result = num1 - num2
-        else:
-            result = num1 * num2
-        count = question(expression, result, counter)
+        count = question(random_expression(), result, counter)
         counter = count
     if counter == 3:
         congratulation()
