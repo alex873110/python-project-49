cde2b3feaacd6ec8b1f32371ccb638042f8b1034
Alexander <83303473+alex873110@users.noreply.github.com>
2023-05-22 13:18:49 +0300

    Update calc_game.py
    change string contcatenation to f-string

--- brain_games/games/calc_game.py
+++ brain_games/games/calc_game.py
@@ -7,7 +7,7 @@ def get_question_data():
     num1 = randint(1, 100)
     num2 = randint(1, 100)
     char = choice('+-*')
-    expression = str(num1) + ' ' + char + ' ' + str(num2)
+    expression = f'{num1} {char} {str(num2)}
     if char == '+':
         result = num1 + num2
     if char == '-':
