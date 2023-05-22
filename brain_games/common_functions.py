7cebc435d33e9f4e58fef90f8355c9dc4e93767c
Alexander <83303473+alex873110@users.noreply.github.com>
2023-05-22 14:34:17 +0300

    Update common_functions.py
    replace concatenation by f-strings

--- brain_games/common_functions.py
+++ brain_games/common_functions.py
@@ -4,12 +4,12 @@ import prompt
 def answer_questions(game):
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
-    print('Hello, ' + name + '!')
+    print(f'Hello, {name}!')
     (expression, result, task) = game()
     print(task)
     counter = 0
     while counter < 3:
-        print('Question: ' + expression)
+        print(f'Question: {expression}')
         ans = prompt.string('Your answer:')
         if ans == str(result):
             print('Correct!')
@@ -17,7 +17,7 @@ def answer_questions(game):
             (expression, result, task) = game()
         else:
             print(f"'{ans}' is wrong answer ;(. Correct answer was '{result}'.")
-            print("Let's try again, " + name + '!')
+            print(f"Let's try again, {name}!")
             break
     if counter == 3:
-        print('Congratulations, ' + name + '!')
+        print(f'Congratulations, {name}!')
