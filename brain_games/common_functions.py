62e435d8ecc1f6290784b961137281479eb4e63b
Alexander <alexzhur87@mail.ru>
2023-05-23 16:34:57 +0300

    refactoring common_functions.py

--- brain_games/common_functions.py
+++ brain_games/common_functions.py
@@ -7,17 +7,14 @@ def answer_questions(game):
     print(f'Hello, {name}!')
     (expression, result) = game.get_question_data()
     print(game.TASK)
-    counter = 0
-    while counter < 3:
+    for i in range(3):
         print(f'Question: {expression}')
         ans = prompt.string('Your answer:')
-        if ans == str(result):
-            print('Correct!')
-            counter = counter + 1
-            (expression, result) = game.get_question_data()
-        else:
+        if ans != str(result):
             print(f"'{ans}' is wrong answer ;(. Correct answer was '{result}'.")
             print(f"Let's try again, {name}!")
             break
-    if counter == 3:
+        print('Correct!')
+        (expression, result) = game.get_question_data()
+    else:
         print(f'Congratulations, {name}!')
