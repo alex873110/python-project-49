4e346d1a816c5233d2a63637526d7a057369b1d4
Alexander <alexzhur87@mail.ru>
2023-05-24 17:07:06 +0300

    refactoring common_functions.py

--- brain_games/common_functions.py
+++ brain_games/common_functions.py
@@ -1,12 +1,11 @@
 import prompt
+ROUNDS_QUANTITY = 3
 
 
 def play_game(game):
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
-    print(game.TASK)
-    ROUNDS_QUANTITY = 3
     for round in range(ROUNDS_QUANTITY):
         expression, result = game.get_question_data()
         print(f'Question: {expression}')
